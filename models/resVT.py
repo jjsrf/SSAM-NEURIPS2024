4f76ed0b86c4e796e35dcde56a0101a7070eb58c
jjsrf <128652654+jjsrf@users.noreply.github.com>
2025-01-02 16:01:03 -0500

    Add files via upload

---
+++ models/resVT.py
@@ -0,0 +1,340 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+import time
+import torch.nn.init as init
+# from utils.common_utils import try_cuda
+# from .init_utils import weights_init
+
+__all__ = ['resVT']  # , 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
+_AFFINE = True
+#_AFFINE = False
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes, affine=_AFFINE)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes, affine=_AFFINE)
+
+        self.downsample = None
+        self.bn3 = None
+        if stride != 1 or in_planes != planes:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False))
+            self.bn3 = nn.BatchNorm2d(self.expansion * planes, affine=_AFFINE)
+
+    def forward(self, x):
+        # x: batch_size * in_c * h * w
+        residual = x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if self.downsample is not None:
+            residual = self.bn3(self.downsample(x))
+        out += residual
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        _outputs = [32, 64, 128]
+        self.in_planes = _outputs[0]
+
+        self.conv1 = nn.Conv2d(3, _outputs[0], kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(_outputs[0], affine=_AFFINE)
+        self.layer1 = self._make_layer(block, _outputs[0], num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, _outputs[1], num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, _outputs[2], num_blocks[2], stride=2)
+        self.linear = nn.Linear(_outputs[2], num_classes)
+
+        self.apply(weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        print(x.shape)
+        out = F.relu(self.bn(self.conv1(x)))
+        out = self.layer1(out)
+        print(out.shape)
+        out = self.layer2(out)
+        print(out.shape)
+        out = self.layer3(out)
+        print(out.shape)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+
+class LayerNormalize(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class MLP_Block(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.1):
+        super().__init__()
+        self.nn1 = nn.Linear(dim, hidden_dim)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.normal_(self.nn1.bias, std=1e-6)
+        self.af1 = nn.GELU()
+        self.do1 = nn.Dropout(dropout)
+        self.nn2 = nn.Linear(hidden_dim, dim)
+        torch.nn.init.xavier_uniform_(self.nn2.weight)
+        torch.nn.init.normal_(self.nn2.bias, std=1e-6)
+        self.do2 = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.nn1(x)
+        x = self.af1(x)
+        x = self.do1(x)
+        x = self.nn2(x)
+        x = self.do2(x)
+
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8, dropout=0.1):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim ** -0.5  # 1/sqrt(dim)
+
+        self.to_qkv = nn.Linear(dim, dim * 3, bias=True)  # Wq,Wk,Wv for each vector, thats why *3
+        torch.nn.init.xavier_uniform_(self.to_qkv.weight)
+        torch.nn.init.zeros_(self.to_qkv.bias)
+
+        self.nn1 = nn.Linear(dim, dim)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.zeros_(self.nn1.bias)
+        self.do1 = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x)  # gets q = Q = Wq matmul x1, k = Wk mm x2, v = Wv mm x3
+        q, k, v = rearrange(qkv, 'b n (qkv h d) -> qkv b h n d', qkv=3, h=h)  # split into multi head attentions
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1, 0), value=True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, float('-inf'))
+            del mask
+
+        attn = dots.softmax(dim=-1)  # follow the softmax,q,d,v equation in the paper
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)  # product of v times whatever inside softmax
+        out = rearrange(out, 'b h n d -> b n (h d)')  # concat heads into one matrix, ready for next encoder block
+        out = self.nn1(out)
+        out = self.do1(out)
+        return out
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, mlp_dim, dropout):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(LayerNormalize(dim, Attention(dim, heads=heads, dropout=dropout))),
+                Residual(LayerNormalize(dim, MLP_Block(dim, mlp_dim, dropout=dropout)))
+            ]))
+
+    def forward(self, x, mask=None):
+        for attention, mlp in self.layers:
+            x = attention(x, mask=mask)  # go to attention
+            x = mlp(x)  # go to MLP_Block
+        return x
+
+class ViTResNet(nn.Module):
+    def __init__(self, block, num_blocks, batch_size=64, num_classes=10, dim=128, num_tokens=8, mlp_dim=256, heads=8,depth=6,emb_dropout=0.1, dropout=0.1):
+        super(ViTResNet, self).__init__()
+        _outputs = [32, 64, 128]
+        self.in_planes = _outputs[0]
+        self.L = num_tokens
+        self.cT = dim
+
+
+
+        self.conv1 = nn.Conv2d(3, _outputs[0], kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(_outputs[0], affine=_AFFINE)
+        self.layer1 = self._make_layer(block, _outputs[0], num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, _outputs[1], num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, _outputs[2], num_blocks[2], stride=2)
+        self.linear = nn.Linear(_outputs[2], num_classes)
+
+        self.apply(weights_init)
+
+        # Tokenization
+        self.token_wA = nn.Parameter(torch.empty(batch_size, self.L, 128),
+                                     requires_grad=True)  # Tokenization parameters
+        torch.nn.init.xavier_uniform_(self.token_wA)
+        self.token_wV = nn.Parameter(torch.empty(batch_size, 128, self.cT),
+                                     requires_grad=True)  # Tokenization parameters
+        torch.nn.init.xavier_uniform_(self.token_wV)
+
+        self.pos_embedding = nn.Parameter(torch.empty(1, (num_tokens + 1), dim))
+        torch.nn.init.normal_(self.pos_embedding, std=.02)  # initialized based on the paper
+
+        # self.patch_conv= nn.Conv2d(64,dim, self.patch_size, stride = self.patch_size)
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, dim))  # initialized based on the paper
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, mlp_dim, dropout)
+
+        self.to_cls_token = nn.Identity()
+
+        self.nn1 = nn.Linear(dim, num_classes)  # if finetuning, just use a linear layer without further hidden layers (paper)
+        torch.nn.init.xavier_uniform_(self.nn1.weight)
+        torch.nn.init.normal_(self.nn1.bias, std=1e-6)
+
+
+
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, img, mask=None):
+        x = F.relu(self.bn(self.conv1(img)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = rearrange(x,
+                      'b c h w -> b (h w) c')  # 64 vectors each with 64 points. These are the sequences or word vecotrs like in NLP
+        # print(x.shape)
+
+        # Tokenization
+        wa = rearrange(self.token_wA, 'b h w -> b w h')  # Transpose
+
+        # print(x.shape, wa.shape)
+        # time.sleep(30)
+
+
+        A = torch.einsum('bij,bjk->bik', x, wa)
+        A = rearrange(A, 'b h w -> b w h')  # Transpose
+        A = A.softmax(dim=-1)
+
+        VV = torch.einsum('bij,bjk->bik', x, self.token_wV)
+        T = torch.einsum('bij,bjk->bik', A, VV)
+        # print(T.size())
+
+        cls_tokens = self.cls_token.expand(img.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, T), dim=1)
+        x += self.pos_embedding
+        x = self.dropout(x)
+        x = self.transformer(x, mask)  # main game
+        x = self.to_cls_token(x[:, 0])
+        x = self.nn1(x)
+
+        return x
+
+        # out = F.avg_pool2d(out, out.size()[3])
+        # out = out.view(out.size(0), -1)
+        # out = self.linear(out)
+        # return out
+
+
+
+def resVT(depth, dataset, batch_size):
+    assert (depth - 2) % 6 == 0, 'Depth must be = 6n + 2, got %d' % depth
+    n = (depth - 2) // 6
+    if dataset == 'cifar10':
+        num_classes = 10
+    elif dataset == 'cifar100':
+        num_classes = 100
+    elif dataset == 'tiny_imagenet':
+        num_classes = 200
+    else:
+        raise NotImplementedError('Dataset [%s] is not supported.' % dataset)
+    return ViTResNet(BasicBlock, [n]*3, batch_size, num_classes)
+
+
+
+
+def weights_init(m):
+    # print('=> weights init')
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        # nn.init.normal_(m.weight, 0, 0.1)
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, nn.Linear):
+        # nn.init.xavier_normal(m.weight)
+        nn.init.normal_(m.weight, 0, 0.01)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        # Note that BN's running_var/mean are
+        # already initialized to 1 and 0 respectively.
+        if m.weight is not None:
+            m.weight.data.fill_(1.0)
+        if m.bias is not None:
+            m.bias.data.zero_()
+
+
+
+
+model = resVT(depth=32, dataset='cifar10', batch_size=1)
+from measure_model import measure_model
+count_ops, count_params, count_conv = measure_model(model, inp_shape=(3, 32, 32))
+print("Flops = %.2f M" % (count_ops / 1e6))
+print("Params = %.2f M" % (count_params / 1e6))
+# i = 0
+# for name, w in model.named_parameters():
+#     if len(w.size()) == 4:
+#         print(name, w.size())
+#         i += 1
+
+#
+# from thop import profile
+# from thop import clever_format
+# input = torch.randn(1, 3, 32, 32)
+# macs, params = profile(model, inputs=(input, ))
+# print(clever_format([macs, params], "%.3f"))
